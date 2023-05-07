1bb3937ec9d283e37cd791dc6c34c68ac5b27e9c
sumitk-hub1999 <skatyal1999@gmail.com>
2023-05-07 18:21:46 +0530

    error

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const passport = require("passport");
 const bcrypt = require("bcrypt");
-const localstrategy = require("passport-local");
-function initialise(passport, getUserByEmail) {
+const localstrategy = require("passport-local").Strategy;
+function initialise(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
     const user = getUserByEmail(email);
 
@@ -9,9 +9,24 @@ function initialise(passport, getUserByEmail) {
       //user not found
       return done(null, false, { message: "user not found" });
     }
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "password incorrect" });
+      }
+    } catch (e) {
+      return done(e);
+    }
   };
-  passport.use(new localstrategy({ userNameField: "email" }), authenticateUser);
+  passport.use(new localstrategy({ userNameField: "email" }, authenticateUser));
 
-  passport.serializeUser((user, done) => {});
-  passport.deserializeUser((id, done) => {});
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id));
+  });
 }
+
+module.exports = initialise;
--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
+const app = express();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
-const passport = require("passport");
-const config=require(./config);
-const app = express();
-
+const flash = require("express-flash");
+const dotenv = require("dotenv");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+const config = require("./config");
+config(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 const users = [];
 app.set("view-engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
+
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get("/", (req, res) => {
   res.render("home.ejs", { name: "css6996" });
 });
@@ -36,5 +60,13 @@ app.post("/register", async (req, res) => {
     console.log(users);
   }
 });
-app.post("/login", (req, res) => {});
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 app.listen(3000);
